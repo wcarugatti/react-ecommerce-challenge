fa82ebd2bc8b4bd24f3de99637e8afe34f1466ad
Wilson Carugatti <wilson.tibia.valentin@gmail.com>
2020-07-27 04:08:15 -0300

    second commit

--- src/components/BaseShop/BaseShop.js
+++ src/components/BaseShop/BaseShop.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from '../Header/Header'
+import {Container} from './style'
 
 export default function BaseShop() {
   return (
-    <div>
+    <Container>
+      <Header/>
       
-    </div>
+    </Container>
   )
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Container} from './style'
+
+export default function Header() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
